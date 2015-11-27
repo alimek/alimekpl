0d15a43604286be70ab865383ebfaa8ad8357147
Grzegorz Mandziak <alimek@alimek.pl>
2015-11-27 19:34:49 +0100

    angular2 app init commit

---
+++ www/src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {AlimekplApp} from './app/alimekpl';
+
+
+bootstrap(AlimekplApp);
---
+++ www/src/app/alimekpl.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'alimekpl-app',
+  providers: [],
+  templateUrl: 'app/alimekpl.html',
+  directives: [],
+  pipes: []
+})
+export class AlimekplApp {
+
+  constructor() {}
+
+}
